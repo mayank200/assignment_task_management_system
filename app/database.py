4e090031589fb31105cd179073d301359f8c3a87
mayank200 <mayanksha200@gmail.com>
2025-04-23 14:29:03 +0530

    changes

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Dependency to get the session
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal() 
     try:
         yield db
     finally:
